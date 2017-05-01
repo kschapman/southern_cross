9a61934dcc699792446324f2f0feecd5d5411b7e
kenneth steven chapman <845869531@kent-202-04m.local>
2017-05-02 10:19:30 +1200

    brunch items added plus skeleton cart start

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+
+  $(".cart-icon").click(function(){
+    $('#cart').show();
+  });
+
+  $(".exit").click(function(){
+    $('#cart').hide();
+  });
+
+  $(".free-range").click(function(){
+    alert("Would you like ")
+  });
+
+
+});
